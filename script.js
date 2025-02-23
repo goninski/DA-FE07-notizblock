cabe7edda4d293d40990c13b32ba22e46fb8a980
goninski <webadmin@goninski.ch>
2025-02-23 08:23:03 +0100

    home url fix

--- script.js
+++ script.js
@@ -150,16 +150,19 @@ function scrollToLocation(location) {
 
 function setHomeURL() {
     let url = new URL(window.location.href);
-    let homeURL = url;
-    switch(url.hostname) {
-        case "127.0.0.1":
-            homeURL = url.origin + '/' + url.pathname.split("/")[1];
-            break
-        case "francois-gonin.developerakademie.net":
-            homeURL = url.origin + '/' + url.pathname.split("/")[1] + '/' + url.pathname.split("/")[2];
-            break
-        default:
-            homeURL = '/';
+    let homeURL = setCondHomeURL(url);
+    let homeLinksRef = document.querySelectorAll('.js-set-home-url');
+    for (let i=0; i < homeLinksRef.length; i++) {
+        homeLinksRef[i].href = homeURL;
     }
-    document.querySelector('.js-set-home-url').href = homeURL;
+}
+
+function setCondHomeURL(url) {
+    if(url.hostname == "127.0.0.1") {
+        return url.origin + '/' + url.pathname.split("/")[1];
+    }
+    if(url.pathname.split("/")[1] == "projekte") {
+        return url.origin + '/' + url.pathname.split("/")[1] + '/' + url.pathname.split("/")[2];
+    }
+    return '/';
 }
